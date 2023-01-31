a1f0376dda53939b94e4776200a8f789995c020a
Berke Öztürk <59619850+Weapfrog@users.noreply.github.com>
2023-01-31 16:49:08 +0300

    .Domain layer update

---
+++ RandomWordGenerator.Domain/Word.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomWordGenerator.Domain
+{
+    public class Word
+    {
+        public int WordID { get; set; }
+        public string WordName { get; set; }
+    }
+}
